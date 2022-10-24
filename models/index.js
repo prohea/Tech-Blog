df7283db6401b4a70c4e2fc1987fb9366911b71d
Mizuri <pro.1.h@outlook.com>
2022-10-24 17:23:45 -0600

    updated indexjs

--- models/index.js
+++ models/index.js
@@ -1,13 +1,30 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Project.belongsTo(User, {
+//Create Associations
+User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = {User, Post, Comment};

b713718e1fe89d428477d7ca0e746f0bb6cace6e
Mizuri <pro.1.h@outlook.com>
2022-10-24 21:45:50 -0600

    updated userroutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -95,3 +95,56 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+//PUT
+router.put('/:id', withAuth, (req,res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+  then(dbUserData => {
+    if(!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+//DELETE
+router.delete('/:id', withAuth, (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  then(dbUserData => {
+    if(!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
